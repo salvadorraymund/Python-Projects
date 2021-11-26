ebbc07339d43964e550ae4ae9344153d4267752e
Raymund Salvador <raymund.martinsalvador@gmail.com>
2021-11-27 05:27:52 +0800

    Fixed issue with Notes page

--- Rest APIs/Flask/People_Part2/people.py
+++ Rest APIs/Flask/People_Part2/people.py
@@ -8,7 +8,7 @@ people data
 
 from flask import make_response, abort
 from config import db
-from models import Person, PersonSchema
+from models import Person, PersonSchema, Note
 
 
 def read_all():
